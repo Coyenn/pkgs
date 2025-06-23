f900ec20db26ab91be055a0e1727c6646cc7de8f
Tim Ritter <timrittermail@gmail.com>
2025-06-23 11:48:36 +0200

    Fix

--- pkgs/moonwave.nix
+++ pkgs/moonwave.nix
@@ -8,14 +8,14 @@ rustPlatform.buildRustPackage rec {
   pname = "moonwave";
   version = "1.3.0";
 
-  repo = fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "evaera";
     repo = pname;
     rev = "v${version}";
     sha256 = "sha256-XCNldIngk+u25NJ8Yn16hhynJ+5uWZM+GD9xww6QD5s=";
   };
-  src = "${repo}/extractor";
 
+  sourceRoot = "${src.name}/extractor";
   cargoHash = "sha256-6o2CMGT7Nyd2594U7/Lkgty9bEM9dVOFBOSd3hTVq0Q=";
   passthru.updateScript = nix-update-script {};
 
