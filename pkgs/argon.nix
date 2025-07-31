e7cb5254a7cbd8341d815fe09da67f263fb2f6bf
Tim Ritter <timrittermail@gmail.com>
2025-07-31 18:18:05 +0200

    Attempt to fix argon

--- pkgs/argon.nix
+++ pkgs/argon.nix
@@ -6,18 +6,18 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "argon";
-  version = "2.0.24";
+  version = "2.0.25";
 
   src = fetchFromGitHub {
     owner = "argon-rbx";
     repo = pname;
     rev = "${version}";
-    sha256 = "sha256-2E9vyXTLCqW5zzCal9FjmV3LvLymjfUbzwZJB77FilU=";
+    sha256 = "sha256-nQdh263qFS3seazdoNxme7SxQ7aJsRmFdoyfsZMDjw0=";
   };
 
-  cargoHash = "sha256-j9aSnyc65CeBdgoFevdn1xpJHs4xWMhFDoRiPizceTI=";
-
+  cargoHash = "sha256-s3/i7RnwadgGBg0lZmttxpLC/hZUba+PGc8WD30aAQI=";
   passthru.updateScript = nix-update-script {};
+  doCheck = false;
 
   meta = with lib; {
     description = "Argon - Full featured tool for Roblox development";
